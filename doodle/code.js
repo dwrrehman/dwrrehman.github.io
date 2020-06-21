ae13a7fd4ac6aa7abca180842f3796eea8e163e8
dwrrehman <dwrrehman@gmail.com>
2020-06-20 17:06:03 -0700

    add the beginning of the doodle web app, that im making for programming teaching purposes.

---
+++ doodle/code.js
@@ -0,0 +1,32 @@
+// a program for doodling on html canvases, using code that uses csr.
+
+
+var canvas = document.getElementById("canvas");
+var draw = canvas.getContext("2d"); 
+
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var center_x = window.innerWidth / 2;
+var center_y = window.innerHeight / 2;
+
+
+
+function inch() {
+
+}
+
+function start() {
+	
+	console.log(Math.sin(4));
+
+	draw.moveTo(center_x,center_y);
+
+	draw.lineTo(50,5);
+	draw.stroke();
+	draw.moveTo(50,5);
+	draw.lineTo(30,40);
+	draw.stroke();
+}
+
+start()
