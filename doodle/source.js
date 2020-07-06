910ce82988230ae93b77aa7f58cfdfa822453b75
dwrrehman <dwrrehman@gmail.com>
2020-07-06 15:31:24 -0700

    add some code to actually make the canvas do something when the user inputs code. for now, its just random. i still need to finish csr.

--- doodle/source.js
+++ doodle/source.js
@@ -1,29 +1,103 @@
 // The Doodle Compiler.
-// Doodle uses Call Signature Resolution to draw on an HTML canvas.
+// Doodle is a language which uses Call Signature Resolution (CSR) 
+// to draw on an HTML canvas, and draw pretty pictures.
+// for the purposes of teaching people how program in a fun way!
+
+
+
+
+
+
 
 // ----------------- DOM setup stuff ------------------------
 
-const center_x = window.innerWidth / 2;
-const center_y = window.innerHeight / 2;
 var canvas = document.getElementById("canvas");
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
+var draw = canvas.getContext("2d"); 
 
 var editor = CodeMirror(document.body, {
-  value: "",
-  mode:  "javascript",
-  lineNumbers: true,
-  tabSize: 4,
-  autofocus: true  
+    value: "",
+    mode:  "",
+    lineNumbers: true,
+    tabSize: 4,
+    autofocus: true  
 });
+
+function random_in(m) {
+    return Math.floor(Math.random() * m) + 1  
+}
+
+/* --------------------- useful code: --------------------
+
+
+    Math.sin(4);
+    Math.cos(4);
+
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+    ctx.beginPath();
+
+    ctx.moveTo(20, 20);
+    ctx.lineTo(20, 100);
+    ctx.lineTo(70, 100);
+
+    ctx.closePath();    
+    ctx.stroke();
+
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+*/
 
 // ----------------- compiler: ---------------------
 
-function csr(context, tokens, count) {
-	var t = tokens.join("");
-	if (t == "repeat") {
+/*
 
-	}
+struct resolved {
+    size_t index;
+    size_t count;
+    size_t begin;
+    size_t done;
+    struct resolved* args;
+};
+
+struct name {
+    size_t type;
+    size_t length;
+    size_t* signature;
+    uint32_t codegen_as;
+    uint32_t precedence;
+};
+
+struct context {
+    size_t best;
+    size_t frame_count;
+    size_t index_count;
+    size_t name_count;
+    size_t* frames;
+    size_t* indicies;
+    struct name* names;
+    struct name* owners;
+};
+
+
+
+*/
+
+function csr(context, tokens, count) {
+
+    for (var i = 0; i < count; i++) {
+
+        if (tokens[i] == 101) {            // just for testing
+
+            var x = random_in(canvas.width);
+            var y = random_in(canvas.height);
+            draw.lineTo(x,y);            
+            draw.stroke();
+        }
+    }	
 }
 
 function compile(text) {
@@ -33,10 +107,11 @@ function compile(text) {
 	var count = 0, l = 1, c = 1;
 
 	for (var i = 0; i < text.length; i++) {
-		if (text[i].charCodeAt(0) > 32) {
+		var k = text[i].charCodeAt(0);
+		if (k > 32) {
 			loc[2 * count] = l;
             loc[2 * count + 1] = c;
-            tokens[count++] = text[i];            
+            tokens[count++] = k;
 		} if (text[i] == '\n') { l++; c = 1; } else c++;
 	}
 
@@ -44,37 +119,18 @@ function compile(text) {
 	csr(context, tokens, count);
 }
 
+function reset_canvas() {        
+    draw.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = canvas.width;
+    draw.moveTo(canvas.width / 2, canvas.height / 2);
+}
 
 editor.on("change", function() {
+    reset_canvas();
 	compile(editor.getValue());
 });
 
-
-// console.log(Math.sin(4));
-
-var draw = canvas.getContext("2d"); 
-draw.moveTo(center_x,center_y);
-draw.lineTo(50,5);
-draw.stroke();
-draw.moveTo(50,5);
-draw.lineTo(30,40);
-draw.stroke();
-
-
-
-
 function info() {
-	alert("User clicked info!!!");
-	console.log("User clicked info!!!");
+	alert("unimplemented. currently just clears the canvas.");
+	reset_canvas();
 }
-
-
-
-
-
-
-
-
-
-
-
