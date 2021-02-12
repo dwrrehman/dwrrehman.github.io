3f24d752cc55a09710a9e1a00f8473b15d922266
dwrrehman <dwrrehman@gmail.com>
2021-02-12 01:15:10 -0800

    redo the interface to the terminal slightly, to make it more ergonomic.

--- terminal.js
+++ terminal.js
@@ -2,33 +2,39 @@ const prompt_emblem = "::"
 
 const welcome_message = "\
 This is the terminal interface for Daniel Rehman's website.<br>\
-Type \"i\" for more info.<br>";
+Type \"a\" for more info.<br>";
 
 const help_message = "\
-l : CLEAR: clear screen <br>\
-i : INFO: this help menu <br>\
-s : LIST: display file <br>\
-d : MOVE: cd <br>\
-w : PWD: print working directory <br>\
-e : EDIT: edit file <br>\
-f : : <br>\
-a : : <br>\n"
+\
+a 	 : INFO: this help menu <br>\
+w 	 : PWD: print working directory <br>\
+e (file) : LIST: display file <br>\
+f (file) : FILE: inspect file <br>\
+\
+j (file) : JUMP: change directory <br>\
+i (file) : INT: unimplemented <br>\
+o (file) : OPEN: unimplemented <br>\
+;	 : CLEAR: clear screen <br>"
 
 const example_html_string = "\
 listing not available right now.<br>\
-<a href='https://www.w3schools.com'>my href here.</a><br>\
+<a href='https://www.w3schools.com'>https://www.w3schools.com</a><br>\
 <img src='dog.JPG' title='this is a dog' width='128' height='128'><br>"
 
 var T = null
 
-var working_directory = "/usr/home/me"
+var working_directory = "/"
 
 function run(command) {
-	if (command === "") return ""; 
-	else if (command === "i") return help_message
-	else if (command === "l") { T.innerHTML = ""; return ""; }
+	if (command === "nbsp;") return "";	
+	else if (command === "a") return help_message
 	else if (command === "w") return working_directory + "<br>";
-	else if (command === "s") return example_html_string;
+	else if (command === "e") return example_html_string;
+	else if (command === "f") return "file-command unimplemented.<br>";
+	else if (command === "j") return "jump-command unimplemented.<br>";
+	else if (command === "i") return "inter command unimplemented.<br>";
+	else if (command === "o") return "open command unimplemented.<br>";
+	else if (command === "l") { T.innerHTML = ""; return ""; }
 	else return "?<br>"
 }
 
@@ -37,8 +43,7 @@ function input(event) {
 	if (event.key === "Enter") {
 		let start = T.innerHTML.lastIndexOf(prompt_emblem) + prompt_emblem.length + 1
 		T.innerHTML = T.innerHTML.slice(0, -15)
-		let end = T.innerHTML.length;
-		let command = T.innerHTML.slice(start, end)
+		let command = T.innerHTML.slice(start, T.innerHTML.length);
 		T.innerHTML += "<br>"
 		let output = run(command)
 		T.innerHTML += output + prompt_emblem + "&nbsp;"
@@ -54,7 +59,6 @@ function input(event) {
 
 window.onload = function() {
 	T = document.getElementById("terminal")
-	T.innerHTML = welcome_message + prompt_emblem + String.fromCharCode(160)
+	T.innerHTML = welcome_message + prompt_emblem + "&nbsp;"
 	document.addEventListener('keyup', input, false);
 }
-
